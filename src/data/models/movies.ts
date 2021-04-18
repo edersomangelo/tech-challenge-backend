4356fa049c440f83193b96e736ceae6000af2537
Ederson Angelo <edersomangelo@gmail.com>
2021-04-18 17:38:33 +0100

    refactor(movie): create a data model to movie entity

---
+++ src/data/models/movies.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+  id?: number
+  name: string
+  synopsis?: string
+  release_date: Date
+  runtime: number
+}
+
+export interface PayloadMovie {
+  id?: Movie['id']
+  name: Movie['name']
+  synopsis?: Movie['synopsis']
+  releasedAt: Movie['release_date']
+  runtime: Movie['runtime']
+}
--- src/lib/movies.ts
+++ src/lib/movies.ts
@@ -1,24 +1,16 @@
 import { knex } from '../util/knex'
-
-export interface Movie {
-  id: number
-  name: string
-  synopsis?: string
-  releasedAt: Date
-  runtime: number
-}
-
+import { Movie } from '../data/models/movies'
 
 export function list(): Promise<Movie[]> {
   return knex.from('movie').select()
 }
 
-export function find(id: number): Promise<Movie> {
+export function find(id: Movie['id']): Promise<Movie> {
   return knex.from('movie').where({ id }).first()
 }
 
 /** @returns whether the ID was actually found */
-export async function remove(id: number): Promise<boolean> {
+export async function remove(id: Movie['id']): Promise<boolean> {
   const count = await knex.from('movie').where({ id }).delete()
   return count > 0
 }
