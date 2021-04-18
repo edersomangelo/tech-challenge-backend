fa5e9ffa835ccdc8a45acb043dd6739510955f3c
Ederson Angelo <edersomangelo@gmail.com>
2021-04-18 17:47:08 +0100

    feat(movie): add create method

--- src/lib/movies.ts
+++ src/lib/movies.ts
@@ -1,5 +1,13 @@
 import { knex } from '../util/knex'
-import { Movie } from '../data/models/movies'
+import { Movie, PayloadMovie } from '../data/models/movies'
+
+const getModel = (payload: PayloadMovie):Movie => ({
+  id: payload.id,
+  name: payload.name,
+  synopsis: payload.synopsis,
+  release_date: payload.releasedAt,
+  runtime: payload.runtime
+})
 
 export function list(): Promise<Movie[]> {
   return knex.from('movie').select()
@@ -13,4 +21,11 @@ export function find(id: Movie['id']): Promise<Movie> {
 export async function remove(id: Movie['id']): Promise<boolean> {
   const count = await knex.from('movie').where({ id }).delete()
   return count > 0
+}
+
+/** @returns the ID that was created */
+export async function create(payload: PayloadMovie): Promise<number> {
+  const model = getModel(payload)
+  const [ id ] = await (knex.into('movie').insert(model))
+  return id
 }
